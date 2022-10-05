e71a743a9b7e2993c084fe7babc4a14feff677ac
JangWoojun <woojun0107@naver.com>
2022-10-05 20:20:53 +0900

    시작 화면 이동

--- app/src/main/java/com/example/clickgame/MainActivity.kt
+++ app/src/main/java/com/example/clickgame/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.clickgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        TODO("화면 넘기고 액티비티 삭제, 뒤로가도 안나오게")
+
+        val startBtn = findViewById<Button>(R.id.startBtn)
+        startBtn.setOnClickListener {
+            val intent = Intent(this,GameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
