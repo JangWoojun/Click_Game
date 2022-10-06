f4ad0ea51a6d150ba7ee127d1daee8e275216838
JangWoojun <woojun0107@naver.com>
2022-10-06 21:28:26 +0900

    랜덤 시간이후 초록색으로 바뀌게 하고 버튼 Click으로 변경 추가

--- app/src/main/java/com/example/clickgame/GameActivity.kt
+++ app/src/main/java/com/example/clickgame/GameActivity.kt
@@ -1,9 +1,13 @@
 package com.example.clickgame
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Button
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import kotlin.concurrent.thread
 
 class GameActivity : AppCompatActivity() {
@@ -13,15 +17,27 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
+        val gameLayout = findViewById<ConstraintLayout>(R.id.gameLayout)
+        val clickBtn = findViewById<Button>(R.id.clickBtn)
+
+        TODO("초록색으로 바뀌고 버튼 클릭하면 시간 뜨게 하고 다시 새 게임 준비")
+
     for (i in 0..5){
         val num = randomNum()
         Handler(Looper.getMainLooper()).postDelayed({
+            start()
+            gameLayout.setBackgroundColor(Color.parseColor("#90ee90"))
+            clickBtn.text = "Click"
 
+            clickBtn.setOnClickListener {
+                stop()
+                Toast.makeText(this,total.toString(),Toast.LENGTH_SHORT).show()
+            }
         }, num.toLong())
 
     }
 
-TODO("랜덤값으로 나온 시간이후 스탑하여 나온 시간 기록 만들기")
+
 
     }
     fun randomNum(): Int {
