e62f4fd7d6fb79a917c6b7628f200d5e1b4a24af
JangWoojun <woojun0107@naver.com>
2022-10-09 23:51:34 +0900

    2022/10/09 버튼 반응속도 텍스트로 알려주는 기능 추가

--- app/src/main/java/com/example/clickgame/GameActivity.kt
+++ app/src/main/java/com/example/clickgame/GameActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -12,8 +13,12 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import kotlin.concurrent.thread
 
 class GameActivity : AppCompatActivity() {
-    var total = 0
+    var time = 0
+    var totalTime = 0
     var started = false
+
+    val TAG = "GameActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
@@ -21,46 +26,54 @@ class GameActivity : AppCompatActivity() {
         val gameLayout = findViewById<ConstraintLayout>(R.id.gameLayout)
         val clickBtn = findViewById<Button>(R.id.clickBtn)
         val count = findViewById<TextView>(R.id.count)
+        val timeCheck = findViewById<TextView>(R.id.timeCheck)
 
-    for (i in 1..5){
-        val num = randomNum()
-        Handler(Looper.getMainLooper()).postDelayed({
-            start()
-            gameLayout.setBackgroundColor(Color.parseColor("#90ee90"))
-            clickBtn.text = "Click"
 
-            clickBtn.setOnClickListener {
-                stop()
-                count.text = "$i/5"
-            }
-        }, num.toLong())
+        for (i in 1..5){
+            val num = (500..5000).random()
+            Log.d(TAG,num.toString())
+            Handler(Looper.getMainLooper()).postDelayed({
+
+                start()
+                gameLayout.setBackgroundColor(Color.parseColor("#90ee90"))
+                timeCheck.setTextColor(Color.parseColor("#90ee90"))
+                clickBtn.text = "Click"
+
+                clickBtn.setOnClickListener {
+                    stop()
+                    count.text = "$i/5"
+                    timeCheck.text = "${time}ms"
+                    timeCheck.setTextColor(Color.parseColor("#ffffff"))
+                    totalTime+=time
+                    time=0
+                }
+
+            }, num.toLong())
+        }
+
 
     }
 
-
-
-    }
-    fun randomNum(): Int {
-        return (1000..4000).random()
-    }
     fun start(){
+        started = true
         thread(start=true) {
 
             while (true){
                 if (!started) break
 
                 Thread.sleep(1)
-                total++
+                time+=1
             }
+
         }
 
     }
+
     fun stop(){
         started=false
         val gameLayout = findViewById<ConstraintLayout>(R.id.gameLayout)
         val clickBtn = findViewById<Button>(R.id.clickBtn)
         gameLayout.setBackgroundColor(Color.parseColor("#FFEA7D"))
         clickBtn.text = "Ready"
-        total = 0
     }
 }
