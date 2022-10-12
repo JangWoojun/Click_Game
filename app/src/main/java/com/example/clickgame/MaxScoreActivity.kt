e7255d1dc7241ee85cbdc9957d28487eb5564b78
JangWoojun <woojun0107@naver.com>
2022-10-12 11:08:23 +0900

    2022/10/12 결과 창 총 합계 평균 값 출력

--- app/src/main/java/com/example/clickgame/MaxScoreActivity.kt
+++ app/src/main/java/com/example/clickgame/MaxScoreActivity.kt
@@ -3,13 +3,17 @@ package com.example.clickgame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MaxScoreActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_max_score)
 
-        val maxScore = intent.getStringExtra("maxScore")?.toInt()
+        val maxScore = intent.getStringExtra("maxScore")
+
+        val maxMs = findViewById<TextView>(R.id.maxMS)
+        maxMs.text = "${maxScore}ms"
 
     }
 }
