34692757edd2b50d3e2bd580e1b116dfa853234c
JangWoojun <woojun0107@naver.com>
2022-10-13 03:42:22 +0900

    2022/10/13 두번 누르면 종료 추가

--- app/src/main/java/com/example/clickgame/MaxScoreActivity.kt
+++ app/src/main/java/com/example/clickgame/MaxScoreActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MaxScoreActivity : AppCompatActivity() {
+    private var backPressedTime : Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_max_score)
@@ -15,5 +16,14 @@ class MaxScoreActivity : AppCompatActivity() {
         val maxMs = findViewById<TextView>(R.id.maxMS)
         maxMs.text = "${maxScore}ms"
 
+    }
+    override fun onBackPressed() {
+
+        if (System.currentTimeMillis() - backPressedTime < 2000) {
+            finish()
+            return
+        }
+
+        backPressedTime = System.currentTimeMillis()
     }
 }
