8e63c2e817d84aefca59c576bc7b5edd22106941
JangWoojun <woojun0107@naver.com>
2022-10-18 14:57:04 +0900

    2022/10/18 게임 종료 시 기다리기 구현

--- app/src/main/java/com/example/clickgame/MultiActivity.kt
+++ app/src/main/java/com/example/clickgame/MultiActivity.kt
@@ -1,7 +1,6 @@
 package com.example.clickgame
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -29,8 +28,8 @@ class MultiActivity : AppCompatActivity() {
     private var started2 = false
     private var chk2 = false
 
-    private var i1 = 1
-    private var i2 = 1
+    private var i1 = 0
+    private var i2 = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,21 +51,14 @@ class MultiActivity : AppCompatActivity() {
 
         }, num2.toLong())
 
+
+
+
         binding.clickBtn1.setOnClickListener {
+
             if (chk1) {
-
-                if (i1 == 5) {
-                    val intent = Intent(this, MultiMaxScoreActivity::class.java)
-                    totalTime1 /= 5
-                    intent.putExtra("maxScore1", totalTime1.toString())
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    startActivity(intent)
-                    overridePendingTransition(0, 0)
-                }
-
-                stop1(i1)
                 i1++
+                stop1(i1)
                 chk1 = false
                 num1 = (3000..5000).random()
                 handler1.postDelayed({
@@ -85,32 +77,41 @@ class MultiActivity : AppCompatActivity() {
                 binding.clickText1.visibility = View.INVISIBLE
                 binding.clickBtn1.visibility = View.INVISIBLE
 
-                handler1.removeCallbacksAndMessages(null);
+                handler1.removeCallbacksAndMessages(null)
                 time1 = 0
                 totalTime1 = 0
                 i1 = 0
-                binding.gameLayout1.setBackgroundColor(Color.parseColor("#a9cbd7"))
+                binding.gameLayout1.setBackgroundResource(R.drawable.border_line_blue_right)
                 binding.count1.text = getString(R.string.count,i1)
                 binding.resetBtn1.setOnClickListener {
                     reset1()
                     i1++
                 }
             }
+            if (i1 == 5) {
+                handler1.removeCallbacksAndMessages(null)
+                binding.gameLayout1.setBackgroundResource(R.drawable.border_line_blue_right)
+                binding.waitText1.text = getString(R.string.wait)
+            }
+            if (i2 == 5 && i1 == 5) {
+                val intent = Intent(this, MultiMaxScoreActivity::class.java)
+                totalTime2 /= 5
+                totalTime1 /= 5
+                intent.putExtra("maxScore1", totalTime1.toString())
+                intent.putExtra("maxScore2", totalTime2.toString())
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                overridePendingTransition(0, 0)
+            }
+
         }
 
         binding.clickBtn2.setOnClickListener {
+
             if (chk2) {
-                if (i2 == 5) {
-                    val intent = Intent(this, MultiMaxScoreActivity::class.java)
-                    totalTime2 /= 5
-                    intent.putExtra("maxScore2", totalTime2.toString())
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    startActivity(intent)
-                    overridePendingTransition(0, 0)
-                }
-                stop2(i2)
                 i2++
+                stop2(i2)
                 chk2 = false
                 num2 = (3000..5000).random()
                 handler2.postDelayed({
@@ -128,25 +129,43 @@ class MultiActivity : AppCompatActivity() {
                 binding.clickText2.visibility = View.INVISIBLE
                 binding.clickBtn2.visibility = View.INVISIBLE
 
-                handler2.removeCallbacksAndMessages(null);
+                handler2.removeCallbacksAndMessages(null)
                 time2 = 0
                 totalTime2 = 0
                 i2 = 0
-                binding.gameLayout2.setBackgroundColor(Color.parseColor("#a9cbd7"))
+                binding.gameLayout2.setBackgroundResource(R.drawable.border_line_blue_left)
                 binding.count2.text = getString(R.string.count,i2)
                 binding.resetBtn2.setOnClickListener {
                     reset2()
                     i2++
                 }
             }
+            if (i2 == 5) {
+                handler2.removeCallbacksAndMessages(null)
+                binding.gameLayout2.setBackgroundResource(R.drawable.border_line_blue_left)
+                binding.waitText2.text = getString(R.string.wait)
+            }
+            if (i2 == 5 && i1 == 5) {
+                val intent = Intent(this, MultiMaxScoreActivity::class.java)
+                totalTime2 /= 5
+                totalTime1 /= 5
+                intent.putExtra("maxScore1", totalTime1.toString())
+                intent.putExtra("maxScore2", totalTime2.toString())
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                overridePendingTransition(0, 0)
+            }
 
         }
+
+
     }
-        fun start1() {
+        private fun start1() {
             binding.player1soon1.visibility = View.INVISIBLE
             binding.player1soon2.visibility = View.INVISIBLE
             binding.resetBtn1.visibility = View.INVISIBLE
-            binding.gameLayout1.setBackgroundColor(Color.parseColor("#2dd12d"))
+            binding.gameLayout1.setBackgroundResource(R.drawable.border_line_green_right)
             binding.timeCheck1.visibility = View.INVISIBLE
             binding.waitText1.visibility = View.INVISIBLE
             binding.clickText1.visibility = View.VISIBLE
@@ -166,10 +185,10 @@ class MultiActivity : AppCompatActivity() {
             }
         }
 
-        fun stop1(i1: Int) {
+        private fun stop1(i1: Int) {
             started1 = false
 
-            binding.gameLayout1.setBackgroundColor(Color.parseColor("#c0102a"))
+            binding.gameLayout1.setBackgroundResource(R.drawable.border_line_red_right)
             binding.clickBtn1.text = getString(R.string.Ready)
             binding.count1.text = getString(R.string.count,i1)
             binding.timeCheck1.text = getString(R.string.timeCheck,time1)
@@ -182,14 +201,14 @@ class MultiActivity : AppCompatActivity() {
             time1 = 0
         }
 
-        fun reset1() {
+        private fun reset1() {
             binding.resetBtn1.visibility = View.INVISIBLE
             binding.player1soon1.visibility = View.INVISIBLE
             binding.player1soon2.visibility = View.INVISIBLE
             binding.clickBtn1.visibility = View.VISIBLE
             binding.waitText1.visibility = View.VISIBLE
 
-            binding.gameLayout1.setBackgroundColor(Color.parseColor("#c0102a"))
+            binding.gameLayout1.setBackgroundResource(R.drawable.border_line_red_right)
 
 
             num1 = (3000..5000).random()
@@ -201,11 +220,11 @@ class MultiActivity : AppCompatActivity() {
             }, num1.toLong())
         }
 
-        fun start2() {
+        private fun start2() {
             binding.player2soon1.visibility = View.INVISIBLE
             binding.player2soon2.visibility = View.INVISIBLE
             binding.resetBtn2.visibility = View.INVISIBLE
-            binding.gameLayout2.setBackgroundColor(Color.parseColor("#2dd12d"))
+            binding.gameLayout2.setBackgroundResource(R.drawable.border_line_green_left)
             binding.timeCheck2.visibility = View.INVISIBLE
             binding.waitText2.visibility = View.INVISIBLE
             binding.clickText2.visibility = View.VISIBLE
@@ -224,10 +243,10 @@ class MultiActivity : AppCompatActivity() {
             }
         }
 
-        fun stop2(i: Int) {
+        private fun stop2(i2: Int) {
             started2 = false
 
-            binding.gameLayout2.setBackgroundColor(Color.parseColor("#c0102a"))
+            binding.gameLayout2.setBackgroundResource(R.drawable.border_line_red_left)
             binding.clickBtn2.text = getString(R.string.Ready)
             binding.count2.text = getString(R.string.count,i2)
             binding.timeCheck2.text = getString(R.string.timeCheck,time2)
@@ -240,14 +259,14 @@ class MultiActivity : AppCompatActivity() {
             time2 = 0
         }
 
-        fun reset2() {
+        private fun reset2() {
             binding.resetBtn2.visibility = View.INVISIBLE
             binding.player2soon1.visibility = View.INVISIBLE
             binding.player2soon2.visibility = View.INVISIBLE
             binding.clickBtn2.visibility = View.VISIBLE
             binding.waitText2.visibility = View.VISIBLE
 
-            binding.gameLayout2.setBackgroundColor(Color.parseColor("#c0102a"))
+            binding.gameLayout2.setBackgroundResource(R.drawable.border_line_red_left)
 
             num2 = (3000..5000).random()
 
