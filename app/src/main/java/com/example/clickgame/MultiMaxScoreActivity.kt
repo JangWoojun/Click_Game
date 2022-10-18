096dd83a92e80db24d56a4b753baecaadf255743
JangWoojun <woojun0107@naver.com>
2022-10-18 12:09:20 +0900

    2022/10/18 최종 승자 보여주기 완성

--- app/src/main/java/com/example/clickgame/MultiMaxScoreActivity.kt
+++ app/src/main/java/com/example/clickgame/MultiMaxScoreActivity.kt
@@ -1,15 +1,44 @@
 package com.example.clickgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.clickgame.databinding.ActivityMaxScoreBinding
+import com.example.clickgame.databinding.ActivityMultiMaxScoreBinding
 
 class MultiMaxScoreActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMultiMaxScoreBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_multi_max_score)
+        binding = ActivityMultiMaxScoreBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val maxScore1 = intent.getStringExtra("maxScore1")
-        val maxScore2 = intent.getStringExtra("maxScore2")
+        val maxScore1 = intent.getStringExtra("maxScore1")!!.toInt()
+        val maxScore2 = intent.getStringExtra("maxScore2")!!.toInt()
+
+        if (maxScore1<maxScore2){
+            binding.maxMS.text = getString(R.string.maxMs,maxScore1.toString())
+        }
+        else {
+            binding.maxMS.text = getString(R.string.maxMs,maxScore2.toString())
+        }
+
+        binding.restartBtn.setOnClickListener {
+            val intent = Intent(this,MultiActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            overridePendingTransition(0, 0)
+        }
+
+        binding.homeBtn.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            overridePendingTransition(0, 0)
+        }
 
     }
 }
